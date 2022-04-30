c08097223c432ea3cc23431fc73febed5fd317ac
Jakob Runge <github@runjak.codes>
2022-04-30 22:57:42 +0200

    draft user/session

---
+++ user/session.ts
@@ -0,0 +1,4 @@
+// Handling for user sessions
+// The idea is to have a store of valid user sessions
+// Upon successful login we want to hand users a (valid) token for their session
+// Sessions may not need persistence
